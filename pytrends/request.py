f0ea71557fb3841e07abd7ee3c9172a0868b04b2
Kirill Krolik <yoursuffer@gmail.com>
2023-03-10 17:35:22 +0300

    fix API change for cookie acquire: GET -> POST

--- pytrends/request.py
+++ pytrends/request.py
@@ -71,7 +71,7 @@ class TrendReq(object):
         """
         while True:
             if "proxies" in self.requests_args:
-                return dict(filter(lambda i: i[0] == 'NID', requests.get(
+                return dict(filter(lambda i: i[0] == 'NID', requests.post(
                     f'{BASE_TRENDS_URL}/?geo={self.hl[-2:]}',
                     timeout=self.timeout,
                     **self.requests_args
@@ -82,7 +82,7 @@ class TrendReq(object):
                 else:
                     proxy = ''
                 try:
-                    return dict(filter(lambda i: i[0] == 'NID', requests.get(
+                    return dict(filter(lambda i: i[0] == 'NID', requests.post(
                         f'{BASE_TRENDS_URL}/?geo={self.hl[-2:]}',
                         timeout=self.timeout,
                         proxies=proxy,
